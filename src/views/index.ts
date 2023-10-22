f5ae94c6d4a035a6f408bf1c6beefd7947b33670
Pedro Nunes <pedronunesjc@gmail.com>
2023-10-22 11:53:48 -0300

    evento do botao funcionando

--- src/views/index.ts
+++ src/views/index.ts
@@ -10,42 +10,54 @@ const rotas = new Rotas();
 const algbuscaLargura = new BuscaLargura(rotas);
 algbuscaLargura.realizaBusca('0x0', '5x0');
 
-const algbuscaProfundidade = new BuscaProfundidade(rotas); 
+const algbuscaProfundidade = new BuscaProfundidade(rotas);
 algbuscaProfundidade.realizaBusca('0x0', '5x0');
 
 const algbuscaGulosa = new BuscaGulosa(rotas);
 algbuscaGulosa.realizaBusca('0x0', '2');
 
 
-// Selecione o botão "Calcular" por seu ID
-const calcularBtn = document.getElementById("calcularBtn");
 
 // Adicione um evento de clique ao botão
-calcularBtn?.addEventListener("click", () => {
-  // Pegue os valores dos selects
-  console.log("Botão de clique foi acionado.");
-
-  const algoritmoElement: HTMLSelectElement | null = document.getElementById("algoritmo") as HTMLSelectElement | null;
-  const prateleiraElement: HTMLSelectElement | null = document.getElementById("prateleira") as HTMLSelectElement | null;
-  console.log("algoritmoElement = " + algoritmoElement);
-  console.log("prateleiraElement = " + prateleiraElement);
-
-  let algoritmo = "";
-  let prateleira = "";
-  if (algoritmoElement && prateleiraElement) {
-    // Agora você pode acessar a propriedade 'value'
-     algoritmo = algoritmoElement.value;
-     prateleira = prateleiraElement.value;
+// Função para lidar com o clique no botão "Calcular"
+// Função para lidar com o clique no botão "Calcular"
+function handleCalcularClick() {
+    // Obtém os elementos dos selects
+    const algoritmoElement = document.getElementById("algoritmo") as HTMLSelectElement | null;
+    const prateleiraElement = document.getElementById("prateleira") as HTMLSelectElement | null;
+  
+    // Obtém os valores dos selects
+    const algoritmo = algoritmoElement?.value || "";
+    const prateleira = prateleiraElement?.value || "";
+  
+    // Verifica o valor do algoritmo
+    if (algoritmo === "0") {
+      // Executar a busca em largura
+      // Substitua este comentário pela lógica da busca em largura
+    } else if (algoritmo === "1") {
+      // Executar a busca em profundidade
+      // Substitua este comentário pela lógica da busca em profundidade
+    } else if (algoritmo === "2") {
+      // Executar a busca gulosa
+      // Substitua este comentário pela lógica da busca gulosa
+    } else {
+      // Algoritmo não selecionado
+      alert("Por favor, selecione um algoritmo válido (0, 1 ou 2).");
+      return;
+    }
+  
+    // Realiza as ações necessárias com os valores
+    const resultado = `Algoritmo escolhido: ${algoritmo}, Prateleira escolhida: ${prateleira}`;
+  
+    // Exibe o resultado na caixa de texto
+    const resultadoElement = document.getElementById("resultado") as HTMLTextAreaElement | null;
+  
+    if (resultadoElement) {
+      resultadoElement.value = resultado;
+    }
   }
-
-  // Realize as ações necessárias com os valores
-  const resultado = `Algoritmo escolhido: ${algoritmo}, Prateleira escolhida: ${prateleira}`;
-
-  // Exiba o resultado na caixa de texto
-  const resultadoElement: HTMLTextAreaElement | null = document.getElementById("resultado") as HTMLTextAreaElement | null;
-
-  if (resultadoElement) {
-    // Agora você pode acessar a propriedade 'value'
-    resultadoElement.value = resultado;
-  }
-});
+  
+  // Adiciona o evento de clique ao botão "Calcular"
+  const calcularBtn = document.getElementById("calcularBtn");
+  calcularBtn?.addEventListener("click", handleCalcularClick);
+  
